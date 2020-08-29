85c327c6b55d60988229b06b376a6396a455e976
Emil Volckmar Ry <emilvry@gmail.com>
2020-08-29 21:29:48 +0200

    own carrier-method for handling matchtimes. stalechecks on adding matches.

--- scheduleMatches.go
+++ scheduleMatches.go
@@ -50,65 +50,75 @@ func makeMatch(rw http.ResponseWriter, req *http.Request) {
 		fmt.Println(err.Error())
 	} else {
 		match.Cvars.Hostname = fmt.Sprintf("matchserver: %s vs %s", match.Team1.Name, match.Team2.Name)
-		json.NewEncoder(rw).Encode(fmt.Sprintf("%s vs %s going to be played on %s schedueled for %s", match.Team1.Name, match.Team2.Name, match.Cvars.Hostname, match.Matchtime))
-		fmt.Sprintf("%s vs %s going to be played on %s schedueled for %s", match.Team1.Name, match.Team2.Name, match.Cvars.Hostname, match.Matchtime)
+		if !matchIsStale(match) {
+			json.NewEncoder(rw).Encode(fmt.Sprintf("%s vs %s going to be played on %s schedueled for %s", match.Team1.Name, match.Team2.Name, match.Cvars.Hostname, match.Matchtime))
+			fmt.Sprintf("%s vs %s going to be played on %s schedueled for %s", match.Team1.Name, match.Team2.Name, match.Cvars.Hostname, match.Matchtime)
 
-		matchConfigs = append(matchConfigs, match)
-	}
-
-}
-
-func unschedueledMatches(rw http.ResponseWriter, req *http.Request) {
-	if len(matchConfigs) == 0 {
-		json.NewEncoder(rw).Encode(false)
-
-	} else {
-		json.NewEncoder(rw).Encode(true)
-
-	}
-}
-
-func scheduleMatches(rw http.ResponseWriter, req *http.Request) {
-	layout := "02-01-2006 15:04:05"
-	for match := range matchConfigs {
-		parsedTime, err := time.Parse(layout, matchConfigs[match].Matchtime)
-		if err != nil {
-			fmt.Println(err)
+			matchConfigs = append(matchConfigs, match)
+		} else {
+			json.NewEncoder(rw).Encode(fmt.Sprintf("%s vs %s which was going to be played on %s is stale, and will not be added to matchconfigs ", match.Team1.Name, match.Team2.Name,match.Matchtime))
 
 		}
+
+
+	}
+}
+
+func matchIsStale(match MatchConfig)(stale bool) {
+		when, err := getMatchTimes(match)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if when < time.Hour {
+			if when < 0 {
+				return true
+			} else {
+				return false
+			}
+	}
+	return false
+}
+
+
+func getMatchTimes(match MatchConfig)(when time.Duration, err error) {
 		layout := "02-01-2006 15:04:05"
-		testTime := matchConfigs[match].Matchtime
-		parsedTime, err = time.Parse(layout, testTime)
+		testTime := match.Matchtime
+		parsedTime, err := time.Parse(layout, testTime)
 		loc, _ := time.LoadLocation("Europe/Oslo")
 		timestamps := time.Date(parsedTime.Year(), parsedTime.Month(), parsedTime.Day(), parsedTime.Hour(), parsedTime.Minute(), parsedTime.Second(), parsedTime.Nanosecond(), loc)
 		if err != nil {
+			return
+		}
+
+		when = time.Until(timestamps)
+		return
+}
+
+
+
+func scheduleMatches(rw http.ResponseWriter, req *http.Request) {
+	if len(matchConfigs) <= 0 {
+		json.NewEncoder(rw).Encode("[ * ] no matches to schedule")
+	}
+	for match := range matchConfigs {
+		matchtime, err := getMatchTimes(matchConfigs[match])
+		if err != nil {
 			fmt.Println(err)
 		}
-
-		now := time.Now()
-		when := time.Until(timestamps)
-
-		if when < time.Hour {
-			if when < 0 {
-				json.NewEncoder(rw).Encode(fmt.Sprintf("[%s * ] Match which was schedueled for %v is stale!", now.In(loc), testTime))
-
-
-			} else {
-				json.NewEncoder(rw).Encode(fmt.Sprintf("[%s * ]  match(%s) needs to be scheduled within the hour: %v till gametime", now.In(loc), testTime, time.Until(timestamps)))
-
-			}
-		} else if when > time.Hour {
-			json.NewEncoder(rw).Encode(fmt.Sprintf("[%s * ] match(%s) %v time\n", now.In(loc), testTime, time.Until(timestamps)))
+		if matchtime < time.Hour {
+			json.NewEncoder(rw).Encode(fmt.Sprintf("%s vs %s needs to be scheduled now(%v)", matchConfigs[match].Team1,matchConfigs[match].Team2, matchtime))
+ 		} else {
+ 			json.NewEncoder(rw).Encode(fmt.Sprintf("%s vs %s (%v) is probably a long way away so we wont worry. yet.", matchConfigs[match].Team1.Name,matchConfigs[match].Team2.Name, matchtime))
 		}
-
 	}
+
+
 }
 
 func main() {
 
 	http.HandleFunc("/makeMatch", makeMatch)
 
-	http.HandleFunc("/unscheduledMatches", unschedueledMatches)
 
 	http.HandleFunc("/scheduleMatches", scheduleMatches)
 
